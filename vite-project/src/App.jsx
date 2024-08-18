73fa2dd5b3e06532b5111a933caec14db6286cf9
Johnathon47 <hei.johnathon.2@gmail.com>
2024-08-19 02:39:40 +0300

    feat: connect my project react in data.json in using fetch.

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('/data.json')
+    .then(response => response.json())
+    .then(json => {
+      setData(json)
+      setLoading(false)
+    })
+    .catch(error => {
+      console.error("Erreur lors du chargement des données:", error)
+      setLoading(false)
+    })
+  }, [])
+  if (loading) {
+    return <p>Chargement des données...</p>
+  }
+
+  if (!data) {
+    return <p>Aucune donnée disponible</p>
+  }
+
+  const personne = data.find(entry => entry.model === "Personne").data
+  const patrimoine = data.find(entry => entry.model === "Patrimoine").data
 
   return (
     <>
@@ -16,7 +40,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      
+      <h1>Information sur {personne.nom}</h1>
     </>
   )
 }
