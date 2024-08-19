211b4fa2e2223cd00987f01ddf6be7916481269f
Johnathon47 <hei.johnathon.2@gmail.com>
2024-08-19 03:06:21 +0300

    feat: construct a table and constract a boucle map for filter a different component on possession

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -31,6 +31,30 @@ function App() {
   return (
     <>
       <h1>Information sur {personne.nom}</h1>
+      <table className='table table-bordered' >
+          <thead>
+            <tr>
+              <th scope='col'>Libelle</th>
+              <th scope='col'>Valeur initiale</th>
+              <th scope='col'>Date de debut</th>
+              <th scope='col'>Date de fin</th>
+              <th scope='col'>Amortissement</th>
+              <th scope='col'>Valeur actuelle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patrimoine.possessions.map((possession, index) => (
+              <tr key={index}>
+              <th scope='row'>{possession.libelle}</th>
+              <td>{possession.valeur}</td>
+              <td>{}</td>
+              <td>{}</td>
+              <td>{}</td>
+              <td>{}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
     </>
   )
 }
