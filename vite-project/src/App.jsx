b0dcd8a970a39a6fc3a3d2db7891fdb38ab1657c
Johnathon47 <hei.johnathon.2@gmail.com>
2024-08-30 00:20:40 +0300

    feat: value possession

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Possession from '../../models/possessions/Possession'
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
 
@@ -27,7 +30,16 @@ function App() {
 
   const personne = data.find(entry => entry.model === "Personne").data
   const patrimoine = data.find(entry => entry.model === "Patrimoine").data
+  const test = new Possession();
 
+  const handleGetValeur = (possession, date) => {
+    if (date) {
+      const dateObject = new Date(date);
+      return possession.getValeur(dateObject);
+    }
+    return 'Non défini';
+  };
+  
   return (
     <>
       <h1>Information sur {personne.nom}</h1>
@@ -38,23 +50,35 @@ function App() {
               <th scope='col'>Valeur initiale</th>
               <th scope='col'>Date de debut</th>
               <th scope='col'>Date de fin</th>
-              <th scope='col'>Amortissement</th>
+              <th scope='col'>Taux d'amortissement</th>
               <th scope='col'>Valeur actuelle</th>
             </tr>
           </thead>
           <tbody>
             {patrimoine.possessions.map((possession, index) => (
+            
               <tr key={index}>
               <th scope='row'>{possession.libelle}</th>
               <td>{possession.valeur}</td>
-              <td>{}</td>
-              <td>{}</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td>{new Date(possession.dateDebut).toUTCString()}</td>
+              <td>{inputValue}</td>
+              <td>{possession.tauxAmortissement}</td>
+              <td>{handleGetValeur(new Possession(
+                null,
+                possession.libelle,
+                possession.valeur,
+                new Date(possession.dateDebut),
+                new Date(inputValue), // Utiliser l'inputValue comme date de fin
+                possession.tauxAmortissement
+              ), inputValue)}</td>
             </tr>
             ))}
           </tbody>
         </table>
+        <input type="Date"
+        value={inputValue}
+        onChange={(e)=>setInputValue(e.target.value)}
+        /> 
     </>
   )
 }
