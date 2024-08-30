208a08f19049ea50001f44172c3e7ce0ffd3e3cb
Johnathon47 <hei.johnathon.2@gmail.com>
2024-08-31 01:45:00 +0300

    feat: create PatrimoineGraph

---
+++ ui/src/pages/PatrimoineGraph.jsx
@@ -0,0 +1,14 @@
+// src/pages/PatrimoineGraph.jsx
+import React from 'react';
+import PatrimoinePieChart from '../components/PatrimoinePieChart';
+import PatrimoineBarChart from '../components/PatrimoineBarChart';
+
+const PatrimoineGraph = () => (
+  <div className="container">
+    <h1>Page du Patrimoine</h1>
+    <PatrimoinePieChart />
+    <PatrimoineBarChart />
+  </div>
+);
+
+export default PatrimoineGraph;
